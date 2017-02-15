c36c3df0538e233c0fa663cae80ba6980a3cb145
Petter Iversen <petteriversen@dhcp-10-22-20-74.wlan.ntnu.no>
2017-02-15 10:31:03 +0100

    update

--- server/api/server/routes/map/index.js
+++ server/api/server/routes/map/index.js
@@ -17,18 +17,21 @@ function createMap (req, res) {
 
 function pointsToLatLonInts(req, res){
   var points = req.body.features[0].geometry.coordinates[0];
-  var intArray = new Uint32Array(points.length*2);
-  console.log(points);
+  const intArray = new Uint32Array((points.length-1)*2);
   var pos = 0;
-  for(var i=0; i<points.length; i++){
+  for(var i=0; i<points.length-1; i++){
     var latAsInt = parseInt(points[i][0] * Math.pow(10, 6));
     var lonAsInt = parseInt(points[i][1] * Math.pow(10, 6));
     intArray[pos] = latAsInt;
     intArray[pos+1] = lonAsInt;
     pos+=2;
   }
-
-  fs.writeFile('binary.orbit', Buffer.from(intArray), 'binary', function(err){
+  console.log('--intarray--');
+  console.log(intArray.buffer);
+  var buf = Buffer.from(intArray.buffer, 0, intArray.buffer.length);
+  console.log('--buffer-- size: '+buf.length);
+  console.log(buf);
+  fs.writeFile('binary.orbit', buf, 'binary', function(err){
     if(err) {
       console.log(err);
     };
@@ -43,11 +46,11 @@ function pointsToLatLonInts(req, res){
 
   var stat = fs.statSync('binary.orbit');
   res.writeHead(200, {
-        'Content-Type': 'text/utf-8',
-        'Content-Length': stat.size
-    });
+    'Content-Type': 'text/utf-8',
+    'Content-Length': stat.size
+  });
 
-    var readStream = fs.createReadStream('binary.orbit');
-    // We replaced all the event handlers with a simple call to readStream.pipe()
-    readStream.pipe(res);
+  var readStream = fs.createReadStream('binary.orbit');
+  // We replaced all the event handlers with a simple call to readStream.pipe()
+  readStream.pipe(res);
 }
