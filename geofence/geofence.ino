fe72c325b5cf524faa31684d1d296e4767fb5e41
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2017-02-22 14:05:03 +0100

    small refactor

--- geofence/geofence.ino
+++ geofence/geofence.ino
@@ -18,9 +18,7 @@
 static NMEAGPS gps;
 struct coordinate fence[20];
 struct coordinate pos;
-int points = 4;
 gps_fix fix;
-
 
 //--------------------------
 
@@ -33,42 +31,32 @@ static void GPSisr( uint8_t c )
 void setup() {
   // Start the normal trace output
   DEBUG_PORT.begin(9600);
-  while (!DEBUG_PORT)
-    ;
-
+  while (!DEBUG_PORT);
   DEBUG_PORT.print( F("NMEA_isr.INO: started\n") );
   DEBUG_PORT.print( F("fix object size = ") );
   DEBUG_PORT.println( sizeof(gps.fix()) );
   DEBUG_PORT.print( F("NMEAGPS object size = ") );
   DEBUG_PORT.println( sizeof(gps) );
   DEBUG_PORT.println( F("Looking for GPS device on " USING_GPS_PORT) );
-
   trace_header( DEBUG_PORT );
-
   DEBUG_PORT.flush();
-
   // Start the UART for the GPS device
   gps_port.attachInterrupt( GPSisr );
   gps_port.begin( 9600 );
 
-  //Square around Trondheim
-  fence[0].latitude = 628927150;
-  fence[0].longitude = 87863160;
-  fence[1].latitude = 628250560;
-  fence[1].longitude = 117965700;
-  fence[2].latitude = 639132280;
-  fence[2].longitude = 118515010;
-  fence[3].latitude = 639277170;
-  fence[3].longitude = 98519900;
-
+  // Import fence here
+  // Set number of points here.
+  
+ 
 }
 
 void doSomeWork() {
     pos.latitude = fix.latitudeL();
     pos.longitude = fix.longitudeL();
     int status = insideFence(fence, pos, points);
-    Serial.println(status);
-  }
+    // Broadcast status
+    
+    
 }
 
 void loop() {
