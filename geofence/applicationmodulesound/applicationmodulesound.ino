48a600f941a6c6903be402afc7f2fa728e8fb337
Einar Hov <einhov@gmail.com>
2017-03-08 10:29:44 +0100

    Send a path as the GPS module simlator

--- geofence/applicationmodulesound/applicationmodulesound.ino
+++ geofence/applicationmodulesound/applicationmodulesound.ino
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include "output.h"
+
 const int gpsInputPort = 7;
 const int speakerPort = 10;
 
@@ -12,13 +14,26 @@ void setup() {
 
 }
 
+char buffer[73];
+
 void loop() {
+	static int i = 0;
 
 	gpsValue = digitalRead(gpsInputPort);
 	if (gpsValue == HIGH) {
 		tone(speakerPort, 1000, 200);
 	}
-  delay(1000);
+
+	delay(1000);
+
+	for(int j = 0; j < 3; j++) {
+		strcpy_P(buffer, (const char*)pgm_read_word(&(nmea_table[i++])));
+		Serial.println(buffer);
+		if(i >= 147) i = 0;
+	}
+
+#if 0
 	Serial.println("$GPGGA,150042,6324.916253,N,01024.387120,E,1,05,2.0,20.0,M,41.0,M,,*71");
 	Serial.println("$GPRMC,150042,A,6324.916253,N,01024.387120,E,0.0,,150217,0.2,W,A*26");
+#endif
 }
